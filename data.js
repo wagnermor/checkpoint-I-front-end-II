b35b75d866cf48f193b88a1e1bc8ed8ce203c143
WagnerMor <wagnermor@outlook.com>
2022-03-27 23:45:39 -0300

    fix: cads objects cards.descricao

--- data.js
+++ data.js
@@ -2,25 +2,25 @@ let cards = [
   {
     destino: 'Santiago',
     valor: '2156',
-    descricao: 'Capital do Chile',
+    descricao: 'Emoldurada pela Cordilheira dos Andes, a capital do Chile é uma metrópole com grandes shoppings, atrações históricas, museus e excelentes opções gastronômicas, caracterizada pelos frutos do mar e os vinhos mundialmente famosos. Santiago também é ponto de partida para conhecer as belezas do Chile, como os picos nevados das montanhas, vinícolas e cidades turísticas como Valparaíso e Viña del Mar. Embarque nessa aventura e se apaixone por esta cidade fascinante.',
     urlImagem:'./img/chile-santiago.jpg'
   },
   {
     destino: 'Motevideo',
     valor: '2053',
-    descricao: 'Capital do Uruguai',
+    descricao: 'Entre as principais atrações da capital uruguaia estão a avenida 18 de Julho, uma das principais do centro de Montevidéu, você encontra dezenas de casas de câmbio, restaurantes e hotéis. Passeie também pelo Mercado do Porto, com lojas de suvenires, a Rambla, que margeia o rio de La Plata, a Praia de Pocitos, o Parque Rodó e o Teatro Sólis, inaugurado em 1856. Faça ainda passeios a cidades vizinhas como Piriapolis, Punta del Este e Colonia del Sacramento.',
     urlImagem:'./img/uruguai-montevideo.jpeg'
   },
   {
     destino: 'Buenos Aires',
     valor: '2068',
-    descricao: 'Capital da Argentina',
+    descricao: 'Uma das principais capitais da América do Sul, Buenos Aires agrada a todos os tipos de público, seja casais em lua de mel procurando por uma viagem romântica, famílias com crianças, grupos de amigos e até mesmo quem viaja sozinho. A língua espanhola e a proximidade com o Brasil facilita o deslocamento pelo país. Outro atrativo é o charme portenho. Os cafés com mesinhas nas calçadas, arquitetura europeia, shows de tango e os restaurantes com o melhor da gastronomia argentina são alguns dos melhores motivos para arrumar as malas!',
     urlImagem:'./img/argentina-buenos-aires.jpg'
   },
   {
     destino: 'Lisboa',
     valor: '5436',
-    descricao: 'Capital de Portugal',
+    descricao: 'Explore castelos, igrejas, restaurantes típicos, praças, museus e edifícios modernos da capital lusitana. Lisboa é um destino para conhecer a história de Portugal e para conferir pontos turísticos com muita história para contar, como a Torre de Belém, o Museu dos Coches, o Mosteiro dos Jerônimos, o Oceanário, entre outros. Aproveite as vantagens de visitar um país europeu sem se preocupar com o idioma e o clima, pois a língua oficial de lá é o português e o clima é ameno durante o ano todo.',
     urlImagem:'./img/portugal-lisboa.jpg'
   }
 
--- main.js
+++ main.js
@@ -72,7 +72,6 @@ function createCard() {
   main.innerHTML = "";
   
   readData();
-  
 }
 
 readData();
